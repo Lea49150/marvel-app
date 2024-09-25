88699f48a67221dbe5bc014ca57ec1244472f5fc
Léa PORCHER <lea.porcher@etu.univ-poitiers.fr>
2024-09-25 17:13:49 +0200

    Update characters list with data

--- src/script.js
+++ src/script.js
@@ -1,15 +1,28 @@
 async function getCharacters() {
     try {
-        const response = await fetch('http://127.0.0.1:5500/src/data/characters.json');
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const characters = await response.json();
-        console.log(characters);
+      const response = await fetch('http://127.0.0.1:5500/src/data/characters.json');
+      
+      if (!response.ok) {
+        throw new Error('Erreur lors de la récupération des données');
+      }
+  
+      const data = await response.json();
+      console.log(data); // Affiche les données dans la console
+
+      // Afficher les noms du JSON en li (à la place du html)
+      // Récupérer l'élément ul avec l'ID "characters"
+      const charactersList = document.getElementById('characters');
+      // Parcourir les données et créer un élément li pour chaque personnage
+      data.forEach(character => {
+        const li = document.createElement('li');
+        li.textContent = character.name; // Assurez-vous que les objets dans le JSON ont un attribut 'name'
+        charactersList.appendChild(li);
+      });
+  
     } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
+      console.error('Erreur:', error);
     }
 }
-
-// Appelle la fonction pour récupérer les données
+  
+  // Appeler la fonction pour récupérer et afficher les données
 getCharacters();
