cc874b66eff2771b7586dab2011ec01f4dfa3f78
Léa PORCHER <lea.porcher@etu.univ-poitiers.fr>
2024-09-30 17:51:45 +0200

    Create CharactersList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,52 @@
+// version 1
 import './App.css'
+import { CharactersList } from './components/CharactersList'
 import characters from './data/characters.json'
+
 
 function App() {
   return (
     <>
       <h1>Marvel Characters</h1>
-      <ul id="characters">
-        {characters.map((character)=> (
-          <li key={character.id}>
-            {character.name}
-            </li>
-        ))}
-      </ul>
+      <CharactersList characters={characters}/>
     </>
   )
 }
 
 export default App
+
+// version 2
+
+
+// import React from "react";
+
+// function App() {
+//   return (<h1>Hello World from react with JSX</h1>);
+// }
+
+// export default App;
+
+// version 3
+// import './App.css'
+
+// function App() {
+//   return (
+//     <>
+//       <h1>Marvel Characters</h1>
+//       <ul id="characters">
+//         <li>
+//           Beast
+//         </li>
+//         <li>
+//           Captain America
+//         </li>
+//         <li>
+//           Deadpool
+//         </li>
+//       </ul>
+//     </>
+//   )
+// }
+
+// export default App
 
