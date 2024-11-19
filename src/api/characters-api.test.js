b26925d5b0696f9f355fac934dc53f93edd525b7
Léa PORCHER <lea.porcher@etu.univ-poitiers.fr>
2024-11-19 17:47:06 +0100

    test api

--- src/api/characters-api.test.js
+++ src/api/characters-api.test.js
@@ -1,4 +1,3 @@
-// FILEPATH: /c:/Users/lporch03/marvel-app/src/api/characters-api.test.js
 import { getCharacters, getCharacterById } from './characters-api';
 import characters from '../data/characters.json';
 
@@ -27,23 +26,23 @@ describe('getCharacters', () => {
     ]);
   });
 
-//   it('should return all characters sorted by modified date in ascending order', () => {
-//     const result = getCharacters('modified', 'asc');
-//     expect(result).toEqual([
-//       { id: 2, name: 'Thor', modified: '2021-01-01' },
-//       { id: 1, name: 'Iron Man', modified: '2021-02-01' },
-//       { id: 3, name: 'Hulk', modified: '2021-03-01' },
-//     ]);
-//   });
+  it('should return all characters sorted by modified date in ascending order', () => {
+    const result = getCharacters('modified', 'asc');
+    expect(result).toEqual([
+      { id: 2, name: 'Thor', modified: '2021-01-01' },
+      { id: 1, name: 'Iron Man', modified: '2021-02-01' },
+      { id: 3, name: 'Hulk', modified: '2021-03-01' },
+    ]);
+  });
 
-//   it('should return all characters sorted by modified date in descending order', () => {
-//     const result = getCharacters('modified', 'desc');
-//     expect(result).toEqual([
-//       { id: 3, name: 'Hulk', modified: '2021-03-01' },
-//       { id: 1, name: 'Iron Man', modified: '2021-02-01' },
-//       { id: 2, name: 'Thor', modified: '2021-01-01' },
-//     ]);
-//   });
+  it('should return all characters sorted by modified date in descending order', () => {
+    const result = getCharacters('modified', 'desc');
+    expect(result).toEqual([
+      { id: 3, name: 'Hulk', modified: '2021-03-01' },
+      { id: 1, name: 'Iron Man', modified: '2021-02-01' },
+      { id: 2, name: 'Thor', modified: '2021-01-01' },
+    ]);
+  });
 });
 
 describe('getCharacterById', () => {
