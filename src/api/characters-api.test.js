9bf7f55ee41dea811a77a7406a69bd4dff063cd3
Léa PORCHER <lea.porcher@etu.univ-poitiers.fr>
2024-11-19 17:40:39 +0100

    changement duplication api test

--- src/api/characters-api.test.js
+++ src/api/characters-api.test.js
@@ -27,23 +27,23 @@ describe('getCharacters', () => {
     ]);
   });
 
-  it('should return all characters sorted by modified date in ascending order', () => {
-    const result = getCharacters('modified', 'asc');
-    expect(result).toEqual([
-      { id: 2, name: 'Thor', modified: '2021-01-01' },
-      { id: 1, name: 'Iron Man', modified: '2021-02-01' },
-      { id: 3, name: 'Hulk', modified: '2021-03-01' },
-    ]);
-  });
+//   it('should return all characters sorted by modified date in ascending order', () => {
+//     const result = getCharacters('modified', 'asc');
+//     expect(result).toEqual([
+//       { id: 2, name: 'Thor', modified: '2021-01-01' },
+//       { id: 1, name: 'Iron Man', modified: '2021-02-01' },
+//       { id: 3, name: 'Hulk', modified: '2021-03-01' },
+//     ]);
+//   });
 
-  it('should return all characters sorted by modified date in descending order', () => {
-    const result = getCharacters('modified', 'desc');
-    expect(result).toEqual([
-      { id: 3, name: 'Hulk', modified: '2021-03-01' },
-      { id: 1, name: 'Iron Man', modified: '2021-02-01' },
-      { id: 2, name: 'Thor', modified: '2021-01-01' },
-    ]);
-  });
+//   it('should return all characters sorted by modified date in descending order', () => {
+//     const result = getCharacters('modified', 'desc');
+//     expect(result).toEqual([
+//       { id: 3, name: 'Hulk', modified: '2021-03-01' },
+//       { id: 1, name: 'Iron Man', modified: '2021-02-01' },
+//       { id: 2, name: 'Thor', modified: '2021-01-01' },
+//     ]);
+//   });
 });
 
 describe('getCharacterById', () => {
